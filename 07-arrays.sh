45fec0ac68550b9982795289ea0471e899780d2a
nikich8 <nikich8@gmail.com>
2023-12-19 21:39:18 +0530

    arrays

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" Mango")
+
+echo "First value: $(FRUITS[0])"
+echo "Second value: $(FRUITS[1])"
+echo "Second value: $(FRUITS[2])"
+echo "All fruits: $(FRUITS[@])"
