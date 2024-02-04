e9ffc82b67c23183eb90cb5f2184b2505988df7a
nikich8 <nikich8@gmail.com>
2024-02-04 13:05:44 +0530

    shell

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,30 +4,8 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: Please run this script with root access"
+    echo "ERROR: Please run this script with root user"
     exit 1
 else
     echo "you are root user"
-fi
-
-yum install mysql -y
-
-if [ $? -ne 0 ]
-then
-    echo "ERROR: Installing myaql is failed"
-    exit 1
-else
-    echo "Installing mysql is success"
-fi   
-
-yum install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "ERROR: installing git is failed"
-    exit 1
-else
-    echo "installing git is success"
-fi        
-
-
+fi 
