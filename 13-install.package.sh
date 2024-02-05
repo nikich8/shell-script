c17e8aaf90981bfe35609fd7583a69acd50131ab
nikich8 <nikich8@gmail.com>
2024-02-05 13:53:12 +0530

    shell

--- 13-install.package.sh
+++ 13-install.package.sh
@@ -4,9 +4,24 @@ ID=($id -u)
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "script started exicuting at $TIMESTAMP" &>> $LOGFILE
+
+
 if [ $ID -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILED $N"
+        
+    else
+        echo -e "$2...$G SUCEESS $N"
+    fi      
+}
 then
     echo  -e "$R ERROR: plese run script with root user $N"
     exit 1
@@ -14,6 +29,20 @@ else
     echo "you are root user"
 fi
 
-echo "All argments paased: $@"
+#echo "All argments paased: $@"
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE #for check packages installed or not
+    if [ $? -ne 0 ] #if not installed  
+    then
+        yum install $package -y &>> $LOGFILE #install 
+        VALIDATE $? "Installation of $package" #validate
+    else
+        echo -e "$package is already installed....$Y SKIPING $N"    
+    fi      
+done
 
+
+
+ 
     
