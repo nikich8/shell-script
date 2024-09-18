9dd6f856d6c6e600a5e9f2478f09a30dfc45ccb1
nikich8 <nikich8@gmail.com>
2024-09-18 13:08:52 +0530

    shell

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,8 +3,9 @@
 ID=$(id -u)
 if [ $ID -ne 0]
 then
-    echo "ERROR: Please run wih roo accoun"
+    echo "ERROR: Please run wih root accoun"
+    
 else
-    echo "you are roo user"
+    echo "you are root user"
 
 fi    
