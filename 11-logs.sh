c0f658b3d2e2aa6503cb7d2ab0ed0fce4a4355d1
nikich8 <nikich8@gmail.com>
2024-09-18 12:49:24 +0530

    shell

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
 
-LOGFILE="/tmp/$0-TIMESTAMP.log"
+TIMESTAMP=$(date -%F-%H-%S-%M)
+LOGFILE="/tmp/$0-$TIMESTAPM.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "ERROR: $2 Installing is $R failed" $N
-        exit 1
-    else
-        echo "$2 Installing is $G success" $N
-fi
-}
-
-if [ $ID -ne 0 ]
+if [ $ID -ne 0]
 then
-    echo "ERROR: Please run this script with root access"
+    echo "ERROR: please run with root account"
     exit 1
 else
     echo "you are root user"
-fi
 
-yum install mysql -y    &>>LOGFILE
-VALIDATE $? "installing mysql"
-
-yum install git -y  &>>LOGFILE
-VALIDATE $? "installing git"
+fi    
